6e1fbd87fc0a0c9af6c4bfed582e10802c827bc4
Richie Casto <rcasto92@gmail.com>
2020-07-26 10:33:41 -0400

    setup very simple site

--- server.js
+++ server.js
@@ -1,10 +1,17 @@
 import express from 'express';
+import path from 'path';
 
 const app = new express();
 const port = process.env.PORT || 3000;
+const __dirname = path.resolve();
+
+app.set('views', path.resolve(__dirname, 'views'));
+app.set('view engine', 'ejs');
+
+app.use(express.static(path.resolve(__dirname, 'public')));
 
 app.get('/', (req, res) => {
-
+    res.render('index');
 });
 
 app.listen(port, () => {
