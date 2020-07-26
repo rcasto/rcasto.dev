f63fa53d9e0459c01a28ea7e1074a693b4557dcc
Richie Casto <rcasto92@gmail.com>
2020-07-26 10:36:38 -0400

    add and use helmet and compression

--- server.js
+++ server.js
@@ -1,4 +1,6 @@
 import express from 'express';
+import helmet from 'helmet';
+import compression from 'compression';
 import path from 'path';
 
 const app = new express();
@@ -8,6 +10,8 @@ const __dirname = path.resolve();
 app.set('views', path.resolve(__dirname, 'views'));
 app.set('view engine', 'ejs');
 
+app.use(helmet());
+app.use(compression());
 app.use(express.static(path.resolve(__dirname, 'public')));
 
 app.get('/', (req, res) => {
