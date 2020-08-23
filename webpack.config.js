1e0588b3cbf67bceb3edc7cd3421d186d0882d1b
Richie Casto <rcasto92@gmail.com>
2020-08-23 00:06:27 -0700

    add style and css loader

--- webpack.config.js
+++ webpack.config.js
@@ -16,4 +16,12 @@ module.exports = {
             ],
         }),
     ],
+    module: {
+        rules: [
+            {
+                test: /\.css$/i,
+                use: ['style-loader', 'css-loader'],
+            },
+        ],
+    },
 };
