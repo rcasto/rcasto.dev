7ac410117c6cf7560b7d7b37b09da6c3e701268b
Richie Casto <rcasto92@gmail.com>
2021-12-30 14:18:55 -0500

    small typo fix up

--- projects/ablab.js
+++ projects/ablab.js
@@ -1,6 +1,6 @@
 const ablab = {
     name: 'ablab',
-    description: 'A simple library for enabling experimentation. Primarily offering experiment bucketing and a defined a schema for experiments using a JSON based config.',
+    description: 'A simple library for enabling experimentation. Primarily offering experiment bucketing and a defined schema for experiments using a JSON based config.',
     links: [{
         text: 'Sample/Demo',
         href: 'https://rcasto.github.io/ablab/index.html'
