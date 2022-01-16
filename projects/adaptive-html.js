3d4e896f616d7c67e027bbecef0e6e0d5fadf859
Richie Casto <rcasto92@gmail.com>
2022-01-16 18:19:31 -0500

    update link for adaptive-editor

--- projects/adaptive-html.js
+++ projects/adaptive-html.js
@@ -3,7 +3,7 @@ const adaptiveHtml = {
     description: 'HTML to Adaptive Card JSON converter library. Mainly targeted at working with the output of WYSIWYG editors.',
     links: [{
         text: 'Sample Editor Integration',
-        href: 'https://adaptive-editor.appspot.com/'
+        href: 'https://rcasto.github.io/adaptive-editor/'
     }, {
         text: 'GitHub',
         href: 'https://github.com/rcasto/adaptive-html'
