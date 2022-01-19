6781b0cf9a396055b5b6248cc6ef3aa8860feb11
Richie Casto <rcasto92@gmail.com>
2022-01-18 21:04:12 -0500

    convert buildView to typescript

--- scripts/buildView.ts
+++ scripts/buildView.ts
@@ -1,27 +1,23 @@
-// const path = require('path');
-// const ejs = require('ejs');
-// const fs = require('fs');
-// const projects = require('../projects/index');
-
 import path from 'path';
 import ejs from 'ejs';
 import fs from 'fs';
-import projects from '../projects';
+import { projects } from '../projects';
+import { IProject } from './schemas';
 
 const indexOutputPath = path.resolve('public');
 const indexView = path.resolve('views/index.ejs');
 
-async function renderView(viewPath, data) {
-    return await ejs.renderFile(viewPath, data);
+async function renderProjects(viewPath: string, projects: IProject[]): Promise<string> {
+    return await ejs.renderFile(viewPath, {
+        projects,
+    });
 }
 
 fs.promises.access(indexOutputPath)
     .catch(() => fs.promises.mkdir(indexOutputPath, {
         recursive: true,
     }))
-    .then(() => renderView(indexView, {
-        projects,
-    }))
+    .then(() => renderProjects(indexView, projects))
     .then(view => fs.promises.writeFile(`${indexOutputPath}/index.html`, view))
     .then(() => console.log('View built and written successfully!'))
     .catch(err => console.error(err));
